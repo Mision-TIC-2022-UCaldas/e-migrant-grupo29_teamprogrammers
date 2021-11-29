5b3606f7c050eb711c01e42d159c413d24671200
CarolinaRoa <yudycaroroab@gmail.com>
2021-11-28 19:36:50 -0500

    Retos 33, 35, 38 y 38

---
+++ Emigrantes/ConexionBD/Conexion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+namespace Emigrantes.ConexionBD
+{
+    public class Conexion
+    {
+        public static SqlConnection Conectar()
+
+        {
+            
+            SqlConnection cn = new SqlConnection("Data Source = CASA; Initial Catalog = Emigrantes; Integrated Security = True");
+            cn.Open();
+            return cn;
+
+        }
+
+       
+
+    }
+}
---
+++ Emigrantes/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Emigrantes.MvcApplication" Language="C#" %>
